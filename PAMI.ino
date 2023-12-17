64a798d3412456203f52a0b280f860feb090dfb2
oXoGus <mdintrat0205@gmail.com>
2023-12-17 12:19:37 +0100

    PAMI V0.2

--- PAMI.ino
+++ PAMI.ino
@@ -17,7 +17,8 @@
 #define trigPin 10
 #define echoPin 11 
 
-
+unsigned char vitesseAvance = 200;
+unsigned char vitesseArret = 0;
 
 Servo servo;
 Ultrasonic ultrasonic(trigPin, echoPin);
@@ -43,22 +44,26 @@ void setup() {
   digitalWrite(pinIN1, 1);
   digitalWrite(pinIN2, 0);
   
+   /*
+    test des moteurs
+   */
+  
   // sens du 2eme 
   digitalWrite(pinIN3, 1);
   digitalWrite(pinIN4, 0);
 
   // vitesse du 1er moteur 
-  analogWrite(pinEN1, 255);
+  analogWrite(pinEN1, vitesseAvance);
 
   // vitesse du 2eme moteur 
-  analogWrite(pinEN2, 255);
+  analogWrite(pinEN2, VitesseAvance);
 
-  delay(300000);
+  delay(3000);
 
   // on arrete les moteurs 
 
-  analogWrite(pinEN1, 0);
-  analogWrite(pinEN2, 0); 
+  analogWrite(pinEN1, vitesseArret);
+  analogWrite(pinEN2, vitesseArret); 
 
 }
 
@@ -67,20 +72,19 @@ void loop() {
     servo.write(angle);
     dist = ultrasonic.Ranging(CM);
     Serial.println(dist);
-    if (dist < 5){
+    while (dist < 5){ // tant que l'on détecte un object a moins de 5 cm on s'arrete 
         // vitesse du 1er moteur 
         analogWrite(pinEN1, 0);
 
         // vitesse du 2eme moteur 
         analogWrite(pinEN2, 0);
     }
-    else{
-        // vitesse du 1er moteur 
-        analogWrite(pinEN1, 255);
+    
+    // vitesse du 1er moteur 
+    analogWrite(pinEN1, vitesseAvance);
 
-        // vitesse du 2eme moteur 
-        analogWrite(pinEN2, 255);
-    }
+    // vitesse du 2eme moteur 
+    analogWrite(pinEN2, vitesseAvance);
     delay(1);
   }
 
@@ -88,20 +92,18 @@ void loop() {
     servo.write(angle);
     dist = ultrasonic.Ranging(CM);
     Serial.println(dist);
-    if (dist < 5){
+    while(dist < 5){ // tant que l'on détecte un object a moins de 5 cm on s'arrete 
         // vitesse du 1er moteur 
-        analogWrite(pinEN1, 0);
+        analogWrite(pinEN1, vitesseArret);
 
         // vitesse du 2eme moteur 
-        analogWrite(pinEN2, 0);
+        analogWrite(pinEN2, vitesseArret);
     }
-    else{
-        // vitesse du 1er moteur 
-        analogWrite(pinEN1, 255);
-
-        // vitesse du 2eme moteur 
-        analogWrite(pinEN2, 255);
-    }
+    // vitesse du 1er moteur 
+    analogWrite(pinEN1, vitesseAvance);
+        
+    // vitesse du 2eme moteur 
+    analogWrite(pinEN2, vitesseAvance);
     delay(1);
   }
 }
