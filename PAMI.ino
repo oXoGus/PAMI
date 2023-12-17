474bcdb2211c364015fdaf5f460907f26b0b5ee8
oXoGus <mdintrat0205@gmail.com>
2023-12-17 12:07:50 +0100

    PAMI V1.0

--- PAMI.ino
+++ PAMI.ino
@@ -1,13 +1,36 @@
+#include <Servo.h>
+#include <Ultrasonic.h>
+// moteur 1
 #define pinIN1 4
 #define pinIN2 2
 #define pinEN1 3
 
+// moteur 2
 #define pinIN3 7 
 #define pinIN4 8
 #define pinEN2 5
 
+// servo moteur 
+#define servoPin 9
+
+// capteur de distance 
+#define trigPin 10
+#define echoPin 11 
+
+
+
+Servo servo;
+Ultrasonic ultrasonic(trigPin, echoPin);
+
+int dist;
 
 void setup() {
+
+  Serial.begin(9600);
+  Serial.println("test");
+
+  servo.attach(servoPin);
+  
   pinMode(pinIN1, OUTPUT);
   pinMode(pinIN2, OUTPUT);
   pinMode(pinEN1, OUTPUT);
@@ -25,12 +48,12 @@ void setup() {
   digitalWrite(pinIN4, 0);
 
   // vitesse du 1er moteur 
-  analogWrite(pinEN1, 128);
+  analogWrite(pinEN1, 255);
 
   // vitesse du 2eme moteur 
-  analogWrite(pinEN2, 128);
+  analogWrite(pinEN2, 255);
 
-  delay(3000);
+  delay(300000);
 
   // on arrete les moteurs 
 
@@ -40,6 +63,45 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(unsigned char angle = 0; angle <= 180; angle++){
+    servo.write(angle);
+    dist = ultrasonic.Ranging(CM);
+    Serial.println(dist);
+    if (dist < 5){
+        // vitesse du 1er moteur 
+        analogWrite(pinEN1, 0);
 
+        // vitesse du 2eme moteur 
+        analogWrite(pinEN2, 0);
+    }
+    else{
+        // vitesse du 1er moteur 
+        analogWrite(pinEN1, 255);
+
+        // vitesse du 2eme moteur 
+        analogWrite(pinEN2, 255);
+    }
+    delay(1);
+  }
+
+  for(unsigned char angle = 180; angle >= 0; angle--){
+    servo.write(angle);
+    dist = ultrasonic.Ranging(CM);
+    Serial.println(dist);
+    if (dist < 5){
+        // vitesse du 1er moteur 
+        analogWrite(pinEN1, 0);
+
+        // vitesse du 2eme moteur 
+        analogWrite(pinEN2, 0);
+    }
+    else{
+        // vitesse du 1er moteur 
+        analogWrite(pinEN1, 255);
+
+        // vitesse du 2eme moteur 
+        analogWrite(pinEN2, 255);
+    }
+    delay(1);
+  }
 }
