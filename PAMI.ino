c6377515145ee969a01e92c7a8719581ed3098f7
oXoGus <mdintrat0205@gmail.com>
2024-02-25 10:04:45 +0100

    version fonctionnel

--- PAMI.ino
+++ PAMI.ino
@@ -1,73 +1,28 @@
-#include <Servo.h>
-#include <Ultrasonic.h>
-// moteur 1
-#define pinIN1 4
-#define pinIN2 2
-#define pinEN1 3
+#include <AFMotor.h>
 
-// moteur 2
-#define pinIN3 7 
-#define pinIN4 8
-#define pinEN2 5
-
-// servo moteur 
-#define servoPin 9
-
-// capteur de distance 
-#define trigPin 10
-#define echoPin 11 
-
-unsigned char vitesseAvance = 200;
-unsigned char vitesseArret = 0;
-
-Servo servo;
-Ultrasonic ultrasonic(trigPin, echoPin);
-
-int dist;
+AF_DCMotor motor1(1); // connecter le moteur1 au port M1 du module Shield L293D
+AF_DCMotor motor2(2);
 
 void setup() {
-
-  Serial.begin(9600);
-  Serial.println("test");
-
-  servo.attach(servoPin);
   
-  pinMode(pinIN1, OUTPUT);
-  pinMode(pinIN2, OUTPUT);
-  pinMode(pinEN1, OUTPUT);
-  pinMode(pinIN3, OUTPUT);
-  pinMode(pinIN4, OUTPUT);
-  pinMode(pinEN2, OUTPUT);
-
-
-  // sens du 1er moteur
-  digitalWrite(pinIN1, 1);
-  digitalWrite(pinIN2, 0);
-  
-   /*
-    test des moteurs
-   */
-  
-  // sens du 2eme 
-  digitalWrite(pinIN3, 1);
-  digitalWrite(pinIN4, 0);
-
-  // vitesse du 1er moteur 
-  analogWrite(pinEN1, vitesseAvance);
-
-  // vitesse du 2eme moteur 
-  analogWrite(pinEN2, VitesseAvance);
-
-  delay(3000);
-
-  // on arrete les moteurs 
-
-  analogWrite(pinEN1, vitesseArret);
-  analogWrite(pinEN2, vitesseArret); 
-
 }
 
 void loop() {
+
+  if 
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  motor1.setSpeed(100); // définir la vitesse du moteur 1
+  motor2.setSpeed(100); // définir la vitesse du moteur 2 
+  // turn on motor
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  
+  delay(3000);  
+  
+  // on eteint le moteur
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  /*
   for(unsigned char angle = 0; angle <= 180; angle++){
     servo.write(angle);
     dist = ultrasonic.Ranging(CM);
@@ -78,6 +33,7 @@ void loop() {
 
         // vitesse du 2eme moteur 
         analogWrite(pinEN2, 0);
+        dist = ultrasonic.Ranging(CM);
     }
     
     // vitesse du 1er moteur 
@@ -98,6 +54,7 @@ void loop() {
 
         // vitesse du 2eme moteur 
         analogWrite(pinEN2, vitesseArret);
+        dist = ultrasonic.Ranging(CM);
     }
     // vitesse du 1er moteur 
     analogWrite(pinEN1, vitesseAvance);
@@ -105,5 +62,5 @@ void loop() {
     // vitesse du 2eme moteur 
     analogWrite(pinEN2, vitesseAvance);
     delay(1);
-  }
+  }*/
 }
